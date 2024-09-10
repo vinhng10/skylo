a3730f1e41c7caadd5c024e3a8aa702fdcf8f174
vinhng10 <qvinhnguyen17@gmail.com>
2024-09-10 21:39:56 +0000

    publisher and consumer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@172.17.0.3:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"vehicle", // name
+		"direct", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	for _, qName := range []string{"entry", "exit"} {
+		q, err := ch.QueueDeclare(
+			qName, // name
+			true,  // durable
+			false, // delete when unused
+			false, // exclusive
+			false, // no-wait
+			nil,   // arguments
+		)
+		failOnError(err, "Failed to declare a queue")
+		err = ch.QueueBind(
+			q.Name,    // queue name
+			qName,     // routing key
+			"vehicle", // exchange
+			false,
+			nil)
+		failOnError(err, "Failed to bind a queue")
+
+		msgs, err := ch.Consume(
+			q.Name, // queue
+			"",     // consumer
+			false,  // auto ack
+			false,  // exclusive
+			false,  // no local
+			false,  // no wait
+			nil,    // args
+		)
+		failOnError(err, "Failed to register a consumer")
+
+		go func() {
+			for msg := range msgs {
+				log.Printf(" [x] %s", msg.Body)
+				msg.Ack(false)
+			}
+		}()
+	}
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+
+	// Keep the main function running to receive messages
+	select {}
+}
---
+++ cmd/publisher/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Event struct {
+	ID           string `json:"id"`
+	VehiclePlate string `json:"vehicle_plate"`
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@172.17.0.3:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"vehile", // name
+		"direct", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	entryQ, err := ch.QueueDeclare(
+		"entry", // name
+		true,    // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare entry queue")
+
+	exitQ, err := ch.QueueDeclare(
+		"exit", // name
+		true,   // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
+	)
+	failOnError(err, "Failed to declare exit queue")
+
+	router := gin.Default()
+	router.POST("/entry", func(c *gin.Context) { postEvent(c, ch, entryQ.Name) })
+	router.POST("/exit", func(c *gin.Context) { postEvent(c, ch, exitQ.Name) })
+
+	router.Run("localhost:8080")
+}
+
+func postEvent(c *gin.Context, ch *amqp.Channel, qName string) {
+	var newEvent Event
+	if err := c.BindJSON(&newEvent); err != nil {
+		return
+	}
+	datetimeKey := fmt.Sprintf("%s_date_time", qName)
+	timedEvent := gin.H{
+		"id":            newEvent.ID,
+		"vehicle_plate": newEvent.ID,
+		datetimeKey:     time.Now(),
+	}
+
+	body, _ := json.Marshal(timedEvent)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := ch.PublishWithContext(ctx,
+		"vehicle", // exchange
+		qName,     // routing key
+		false,     // mandatory
+		false,     // immediate
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "application/json",
+			Body:         body,
+		})
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Failed to publish a message"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, timedEvent)
+}
