d02da1e6e1f09543b3eaea95b3d1af82982681fa
vinhng10 <qvinhnguyen17@gmail.com>
2024-09-13 09:37:03 +0000

    add prometheus

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/redis/go-redis/v9"
 )
@@ -24,6 +26,25 @@ type Event struct {
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func timer(job string, histogram prometheus.Histogram) func() {
+	start := time.Now()
+	return func() {
+		// Measure the elapsed time and observe it in the histogram
+		histogram.Observe(time.Since(start).Seconds())
+
+		// Push the histogram data to the Prometheus Pushgateway
+		if err := push.New(fmt.Sprintf(
+			"http://%s:%s",
+			os.Getenv("PUSHGATEWAY_HOST"),
+			os.Getenv("PUSHGATEWAY_PORT"),
+		), job).
+			Collector(histogram).
+			Push(); err != nil {
+			log.Printf("Could not push to Pushgateway: %v", err)
+		}
 	}
 }
 
@@ -68,92 +89,112 @@ func sendEvent(ch *amqp.Channel, vehiclePlate string, qName string) error {
 	return err
 }
 
-func generateEntry(ch *amqp.Channel, rdb *redis.Client, qName string) {
+func produceEntry(ch *amqp.Channel, rdb *redis.Client, qName string) {
+	// Create a Prometheus Histogram to track the duration
+	duration := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "produce_entry",
+		Help: "Time taken to produce vehicle entry",
+	})
+
 	for {
-		vehiclePlate := generateRandomVehiclePlate()
+		func() {
+			// Push time metric to Prometheus PushGateway
+			defer timer("produce_entry", duration)
 
-		exists, err := func() (int64, error) {
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancel()
+			vehiclePlate := generateRandomVehiclePlate()
 
-			exists, err := rdb.Exists(ctx, vehiclePlate).Result()
-			return exists, err
-		}()
-		if err != nil {
-			log.Printf("Failed to check vehicle plate existence: %v\n", err)
-			continue
-		}
-
-		if exists == 0 {
-			err := func() error {
+			exists, err := func() (int64, error) {
 				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 				defer cancel()
-				_, err := rdb.Set(ctx, vehiclePlate, 1, 0).Result()
-				return err
+
+				exists, err := rdb.Exists(ctx, vehiclePlate).Result()
+				return exists, err
 			}()
 			if err != nil {
-				log.Printf("Failed to set vehicle plate to redis: %v\n", err)
-				continue
+				log.Printf("Failed to check vehicle plate existence: %v\n", err)
+				return
 			}
 
-			err = sendEvent(ch, vehiclePlate, qName)
-			if err != nil {
-				log.Printf("Failed to send event: %v\n", err)
-				continue
-			}
-		} else {
-			log.Printf("Vehicle plate already exists: %s", vehiclePlate)
-		}
+			if exists == 0 {
+				err := func() error {
+					ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+					defer cancel()
+					_, err := rdb.Set(ctx, vehiclePlate, 1, 0).Result()
+					return err
+				}()
+				if err != nil {
+					log.Printf("Failed to set vehicle plate to redis: %v\n", err)
+					return
+				}
 
-		waitTime := rand.Intn(10) + 1
-		time.Sleep(time.Duration(waitTime) * time.Second)
+				err = sendEvent(ch, vehiclePlate, qName)
+				if err != nil {
+					log.Printf("Failed to send event: %v\n", err)
+					return
+				}
+			} else {
+				log.Printf("Vehicle plate already exists: %s", vehiclePlate)
+			}
+		}()
+
+		time.Sleep(time.Duration(rand.Intn(10)+1) * time.Second)
 	}
 }
 
-func generateExit(ch *amqp.Channel, rdb *redis.Client, qName string) {
+func produceExit(ch *amqp.Channel, rdb *redis.Client, qName string) {
+	// Create a Prometheus Histogram to track the duration
+	duration := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "produce_exit",
+		Help: "Time taken to produce vehicle exit",
+	})
+
 	for {
-		var vehiclePlate string
-		var err error
+		func() {
+			// Push time metric to Prometheus PushGateway
+			defer timer("produce_exit", duration)
 
-		if rand.Float32() < 0.8 {
-			vehiclePlate, err = func() (string, error) {
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
+			var vehiclePlate string
+			var err error
 
-				vehiclePlate, err := rdb.RandomKey(ctx).Result()
-				return vehiclePlate, err
-			}()
-			if err != nil {
-				continue
-			}
-		} else {
-			vehiclePlate = generateRandomVehiclePlate()
-		}
+			if rand.Float32() < 0.8 {
+				vehiclePlate, err = func() (string, error) {
+					ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+					defer cancel()
 
-		if vehiclePlate != "" {
-			err = sendEvent(ch, vehiclePlate, qName)
-			if err != nil {
-				log.Printf("Failed to send event: %v\n", err)
-				continue
+					vehiclePlate, err := rdb.RandomKey(ctx).Result()
+					return vehiclePlate, err
+				}()
+				if err != nil {
+					return
+				}
+			} else {
+				vehiclePlate = generateRandomVehiclePlate()
 			}
 
-			err := func() error {
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
+			if vehiclePlate != "" {
+				err = sendEvent(ch, vehiclePlate, qName)
+				if err != nil {
+					log.Printf("Failed to send event: %v\n", err)
+					return
+				}
 
-				_, err := rdb.Del(ctx, vehiclePlate).Result()
-				return err
-			}()
-			if err != nil {
-				log.Printf("Failed to delete vehicle plate in redis: %v\n", err)
-				continue
+				err := func() error {
+					ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+					defer cancel()
+
+					_, err := rdb.Del(ctx, vehiclePlate).Result()
+					return err
+				}()
+				if err != nil {
+					log.Printf("Failed to delete vehicle plate in redis: %v\n", err)
+					return
+				}
+			} else {
+				log.Printf("No vehicle")
 			}
-		} else {
-			log.Printf("No vehicle")
-		}
+		}()
 
-		waitTime := rand.Intn(10) + 1
-		time.Sleep(time.Duration(waitTime) * time.Second)
+		time.Sleep(time.Duration(rand.Intn(10)+1) * time.Second)
 	}
 }
 
@@ -213,8 +254,8 @@ func main() {
 	)
 	failOnError(err, "Failed to declare exit queue")
 
-	go generateEntry(ch, rdb, entryQ.Name)
-	go generateExit(ch, rdb, exitQ.Name)
+	go produceEntry(ch, rdb, entryQ.Name)
+	go produceExit(ch, rdb, exitQ.Name)
 
 	// Keep the main function running to receive messages
 	select {}
